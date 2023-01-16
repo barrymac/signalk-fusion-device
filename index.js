0edbefc4e6fd96335603d8ebdbb775463f73d0e4
Barry MacMahon <barrymac@gmail.com>
2023-01-16 00:01:14 -0300

    created a version that might work for volume and mute for multiple sound cards with alsa

--- index.js
+++ index.js
@@ -1,56 +1,79 @@
 const plugin = require('@signalk/signalk-plugin-fusion-audio');
-const { PGN } = plugin;
-const { PGN, sendCommand, sendProductInformation } = require('@signalk/signalk-plugin-fusion-audio');
-var lastVolume = 0;
+const {PGN} = plugin;
+const {sendProductInformation} = require('@signalk/signalk-plugin-fusion-audio');
+const {exec} = require('child_process');
 var lastMute = false;
-var lastSource = '';
+var desiredZone = 0; // define desired zone here
+var soundCardId = 0; // define sound card id here
 
-
-module.exports = function(app) {
+module.exports = function (app) {
 // send a product information message to identify the plugin as a Fusion audio device
-  sendProductInformation(app, {
-    PIDIL: 0x10, // change this to a unique address on your network
-    PIDIH: 0x00,
-    DF: 0x06, // audio function
-    DC: 0x14, // audio device class
-    FirmwareVersion: 0x01,
-    ModelId: "SK-AUDIO-PLUGIN",
-    ModelVersion: "1.0",
-    DF: 0x08, // remote control function
-    DF: 0x09, // equalization function
-    DF: 0x0A, // tone control function
-    DF: 0x0B, // balance and fader function
-    DC: 0x15, // multi-zone audio device class
-    DC: 0x17, // audio processor device class
-   });
-
-  subscribe(app, function(msg) {
-    // other logic here to handle other PGN's like volume control
-  });
-};
-
+    sendProductInformation(app, {
+        PIDIL: 0x10, // change this to a unique address on your network
+        PIDIH: 0x00,
+        DF: [0x06, 0x08, 0x09, 0x0A, 0x0B], // audio function, remote control function, equalization function, tone control function, balance and fader function
+        DC: [0x14, 0x15, 0x17], // audio device class, multi-zone audio device class, audio processor device class
+        FirmwareVersion: 0x01,
+        ModelId: "SK-AUDIO-PLUGIN",
+        ModelVersion: "1.0",
+    });
 
 // Listen for volume command PGN
-server.on(`nmea2000out`, (n2k) => {
-  if (n2k.pgn === PGN.VolumeCommand) {
-    lastVolume = n2k.fields.Volume;
-    console.log(`Volume changed to: ${lastVolume}`);
-  }
-});
+    app.on(nmea2000out, (n2k) => {
+        if (n2k.pgn === PGN.VolumeCommand) {
+// check if the command is for the desired zone
+            if (n2k.fields.Zone === desiredZone) {
+// change the volume for the specific sound card
+                exec(`amixer -c ${soundCardId} set PCM ${n2k.fields.Volume}%`, (error, stdout, stderr) => {
+                    if (error) {
+                        console.log(`error: ${error.message}`);
+                        return;
+                    }
+                    if (stderr) {
+                        console.log(`stderr: ${stderr}`);
+                        return;
+                    }
+                    console.log(`stdout: ${stdout}`);
+                });
+            }
+        }
+    });
 
 // Listen for mute command PGN
-server.on(`nmea2000out`, (n2k) => {
-  if (n2k.pgn === PGN.MuteCommand) {
-    lastMute = n2k.fields.Mute;
-    console.log(`Mute changed to: ${lastMute}`);
-  }
-});
-
-// Listen for source command PGN
-server.on(`nmea2000out`, (n2k) => {
-  if (n2k.pgn === PGN.SourceCommand) {
-    lastSource = n2k.fields.Source;
-    console.log(`Source changed to: ${lastSource}`);
-  }
-});
-
+    app.on(nmea2000out, (n2k) => {
+        if (n2k.pgn === PGN.MuteCommand) {
+            lastMute = n2k.fields.Mute;
+            console.log(`Mute changed to: ${lastMute}`);
+// check if the command is for the desired zone
+            if (n2k.fields.Zone === desiredZone) {
+// mute or unmute the specific sound card
+                if (lastMute) {
+                    exec(`amixer -c ${soundCardId} set PCM mute`, (error, stdout, stderr) => {
+                        if (error) {
+                            console.log(`error: ${error.message}`);
+                            return;
+                        }
+                        if (stderr) {
+                            console.log(`stderr: ${stderr}`);
+                            return;
+                        }
+                        console.log(`stdout: ${stdout}`);
+                    });
+                } else {
+                    exec(`amixer -c ${soundCardId} set PCM unmute`, (error, stdout, stderr) => {
+                        if (error) {
+                            console.log(`error: ${error.message}`);
+                            return;
+                        }
+                        if (stderr) {
+                            console.log(`stderr: ${stderr}`);
+                            return;
+                        }
+                        console.log(`stdout: ${stdout}`)
+                        ;
+                    });
+                }
+            }
+        }
+    });
+};
